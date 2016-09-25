87c4fdc34ed88cd9e89445d47256a5c7b105c4d3
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-09-25 16:44:33 -0700

    draw ship to canvas

--- JS/scripts.js
+++ JS/scripts.js
@@ -8,7 +8,7 @@ function canvas() {
     ctx.rect(0, 0, 300, 300);
     ctx.fill();
     stars();
-    //makeShip();
+    makeShip();
   }
 }
 
@@ -17,12 +17,10 @@ function stars() {
     var x = Math.floor(Math.random() * 299);
     var y = Math.floor(Math.random() * 299);
     //for randomizing colors
-    var r = Math.floor(Math.random() * (256 - 100) + 100);
-    var g = Math.floor(Math.random() * (256 - 100) + 100);
+    var r = Math.floor(Math.random() * (230 - 60) + 60);
+    //var g = Math.floor(Math.random() * (256 - 100) + 100);
     var b = Math.floor(Math.random() * (256 - 100) + 100);
-    console.log("rgb(" + r + "," + g + "," + b + ")");
-    ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-    //ctx.fillStyle = "white";
+    ctx.fillStyle = "rgb(" + r + "," + r + "," + b + ")";
 
     if (x < 30 || y < 30) ctx.fillStyle = "black";
 
@@ -34,4 +32,26 @@ function stars() {
 }
 
 function makeShip() {
+  console.log(123);
+  // Draw saucer bottom.
+  ctx.beginPath();
+  ctx.moveTo(28.4, 16.9);
+  ctx.bezierCurveTo(28.4, 19.7, 22.9, 22.0, 16.0, 22.0);
+  ctx.bezierCurveTo(9.1, 22.0, 3.6, 19.7, 3.6, 16.9);
+  ctx.bezierCurveTo(3.6, 14.1, 9.1, 11.8, 16.0, 11.8);
+  ctx.bezierCurveTo(22.9, 11.8, 28.4, 14.1, 28.4, 16.9);
+  ctx.closePath();
+  ctx.fillStyle = "rgb(160, 160, 160)";
+  ctx.fill();
+
+  // Draw saucer top.
+  ctx.beginPath();
+  ctx.moveTo(22.3, 12.0);
+  ctx.bezierCurveTo(22.3, 13.3, 19.4, 14.3, 15.9, 14.3);
+  ctx.bezierCurveTo(12.4, 14.3, 9.6, 13.3, 9.6, 12.0);
+  ctx.bezierCurveTo(9.6, 10.8, 12.4, 9.7, 15.9, 9.7);
+  ctx.bezierCurveTo(19.4, 9.7, 22.3, 10.8, 22.3, 12.0);
+  ctx.closePath();
+  ctx.fillStyle = "rgb(51, 190, 0)";
+  ctx.fill();
 }
